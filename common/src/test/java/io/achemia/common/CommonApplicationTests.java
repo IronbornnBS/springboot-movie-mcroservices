dd3f08892e75a030435f72ad207a1e2d5e3db48d
IronbornnBS <mabuza6@gmail.com>
2023-08-02 10:31:09 +0200

    initial common module

---
+++ common/src/test/java/io/achemia/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package io.achemia.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
