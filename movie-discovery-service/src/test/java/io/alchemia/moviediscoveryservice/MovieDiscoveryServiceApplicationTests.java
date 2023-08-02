3364267683b529fbeeb67f11191c9967117d4e45
IronbornnBS <mabuza6@gmail.com>
2023-08-02 09:48:39 +0200

    initial service discovery

---
+++ movie-discovery-service/src/test/java/io/alchemia/moviediscoveryservice/MovieDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.alchemia.moviediscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
