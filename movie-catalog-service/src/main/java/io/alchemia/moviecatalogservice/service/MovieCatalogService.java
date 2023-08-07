251b7a026e7e443f4c531161b6836dd291a83680
IronbornnBS <mabuza6@gmail.com>
2023-08-07 17:11:10 +0200

    refactored code to used service discovery

--- movie-catalog-service/src/main/java/io/alchemia/moviecatalogservice/service/MovieCatalogService.java
+++ movie-catalog-service/src/main/java/io/alchemia/moviecatalogservice/service/MovieCatalogService.java
@@ -4,47 +4,46 @@ import io.alchemia.moviecatalogservice.model.CatalogItem;
 import io.alchemia.moviecatalogservice.model.Movie;
 import io.alchemia.moviecatalogservice.model.UserRating;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-import reactor.util.retry.Retry;
-
-import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class MovieCatalogService {
 
-    public static final int TIMEOUT_SECONDS = 3;
-    public static final int MAX_ATTEMPTS = 3;
-
     @Value("${info-service.movie-info}")
     private String movieInfoUrl;
 
     @Value("${rating-service.user-rating}")
     private String userRatingUrl;
-    private final WebClient.Builder webClientBuilder;
 
-    public MovieCatalogService(final WebClient.Builder webClientBuilder) {
-        this.webClientBuilder = webClientBuilder;
+    private final WebClient.Builder builder;
+
+    public MovieCatalogService(WebClient.Builder builder) {
+        this.builder = builder;
     }
 
     public List<CatalogItem> getCatalog(String userId) {
 
-        UserRating userRating =  getWebClientBuilder(HttpMethod.GET,
-                userRatingUrl.concat(userId),
-                UserRating.class);
+        UserRating userRating = builder
+                .build()
+                .get()
+                .uri(userRatingUrl+userId)
+                .retrieve()
+                .bodyToMono(UserRating.class)
+                .block();
 
         return userRating.getRatings()
                 .stream()
                 .map( rating -> {
-                    Movie movie = getWebClientBuilder(HttpMethod.GET,
-                            movieInfoUrl.concat(rating.getId()),
-                            Movie.class);
+                    Movie movie = builder
+                            .build()
+                            .get()
+                            .uri(movieInfoUrl + rating.getId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();
 
                     return CatalogItem.builder()
                             .name(movie.getName())
@@ -53,22 +52,5 @@ public class MovieCatalogService {
                             .build();
                 })
                 .collect(Collectors.toList());
-    }
-
-    private <T> T getWebClientBuilder(HttpMethod httpMethod,
-                                      String url,
-                                      Class<T> responseDtoClass) {
-
-        return webClientBuilder.build()
-                .method(httpMethod)
-                .uri(url)
-                .retrieve()
-                .bodyToMono(responseDtoClass)
-                .retryWhen(Retry.backoff(MAX_ATTEMPTS, Duration.ofSeconds(TIMEOUT_SECONDS))
-                        .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> {
-                            throw new RuntimeException(retrySignal.failure());
-                        })
-                )
-                .block();
     }
 }
--- movie-info-service/src/main/java/io/alchemia/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/io/alchemia/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,9 +2,11 @@ package io.alchemia.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MovieInfoServiceApplication {
 
