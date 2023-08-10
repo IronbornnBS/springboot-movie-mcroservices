cafa9b6b42f3437cd380aca9547267d6b9b3c810
IronbornnBS <mabuza6@gmail.com>
2023-08-10 13:56:33 +0200

    identity service init

---
+++ movie-identity-service/src/main/java/io/alchemia/movieidentityservice/MovieIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package io.alchemia.movieidentityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieIdentityServiceApplication.class, args);
+	}
+
+}
---
+++ movie-identity-service/src/test/java/io/alchemia/movieidentityservice/MovieIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.alchemia.movieidentityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieIdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
